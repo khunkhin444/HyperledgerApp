e2f7cca6716bc96d4e32dd8a97cb81ed9eb964a8
Tony <khunkhin444@gmail.com>
2023-09-26 14:02:44 +0800

    Change files.js, server.js

--- backend/routes/files.js
+++ backend/routes/files.js
@@ -1,25 +1,12 @@
-// Handle file-related transactions.
-
 const express = require('express');
 const multer = require('multer');
 const storage = multer.memoryStorage(); // Store the file in memory
 const upload = multer({ storage: storage });
 const router = express.Router();
-const { Gateway, Wallets } = require('fabric-network');
 
-const initializeWallet = async () => {
-  return await Wallets.newFileSystemWallet('./wallet');
-};
-
-router.post('/sendFile', upload.single('file'), async (req, res) => {
-  const wallet = await initializeWallet();
-  const gateway = new Gateway();
-
+router.post('/sendFile', upload.single('file'), async (req, res) => { //Replace the /sendFile route with actual functioned chaincode
   try {
-    await gateway.connect('./config/connection-profile.json', { wallet, identity: 'userId' });
-
-    const network = await gateway.getNetwork('mychannel');
-    const contract = network.getContract('mycontract');
+    const contract = req.contract;
     const fileBuffer = req.file.buffer;
     await contract.submitTransaction('sendFile', fileBuffer.toString('base64'));
 
@@ -27,40 +14,25 @@ router.post('/sendFile', upload.single('file'), async (req, res) => {
 
   } catch (error) {
     res.status(500).json({ success: false, message: error.message });
-  } finally {
-    gateway.disconnect();
   }
 });
 
-router.get('/downloadFile', async (req, res) => {
-  const wallet = await initializeWallet();
-  const gateway = new Gateway();
-
+router.get('/downloadFile', async (req, res) => { //Replace the /downloadFile route with actual functioned chaincode
   try {
-    // await gateway.connect('./config/connection-profile.json', { wallet, identity: 'userId' });
-    // File download logic here. Retrieve file from the blockchain using Hyperledger Fabric SDK.
-    const network = await gateway.getNetwork('mychannel');
-    const contract = network.getContract('mycontract');
-    const result = await contract.evaluateTransaction('downloadFile', 'fileId');
+    const contract = req.contract;
+    const result = await contract.evaluateTransaction('downloadFile', 'fileId');  // Replace 'fileId' with the actual ID you use
     const fileBuffer = Buffer.from(result, 'base64');
 
     res.status(200).send(fileBuffer);
 
   } catch (error) {
     res.status(500).json({ success: false, message: error.message });
-  } finally {
-    gateway.disconnect();
   }
 });
 
-router.post('/sendBackFile', upload.single('file'), async (req, res) => {
-  const wallet = await initializeWallet();
-  const gateway = new Gateway();
-
+router.post('/sendBackFile', upload.single('file'), async (req, res) => { //Replace the /sendBackFile route with actual functioned chaincode
   try {
-    await gateway.connect('./config/connection-profile.json', { wallet, identity: 'userId' });
-    const network = await gateway.getNetwork('mychannel');
-    const contract = network.getContract('mycontract');
+    const contract = req.contract;
     const fileBuffer = req.file.buffer;
     await contract.submitTransaction('sendBackFile', fileBuffer.toString('base64'));
 
@@ -68,8 +40,6 @@ router.post('/sendBackFile', upload.single('file'), async (req, res) => {
 
   } catch (error) {
     res.status(500).json({ success: false, message: error.message });
-  } finally {
-    gateway.disconnect();
   }
 });
 
--- backend/server.js
+++ backend/server.js
@@ -2,46 +2,55 @@ const express = require('express');
 const app = express();
 const authRoutes = require('./routes/auth');
 const fileRoutes = require('./routes/files');
-const mydatabase = require('./db');  // Importing the CouchDB database instance
+const mydatabase = require('./db'); // Importing the CouchDB database instance
 const { Gateway, Wallets } = require('fabric-network');
 const fs = require('fs'); // Node.js file system module
 const cors = require("cors");
 app.use(cors());
 
+let gateway;
+let contract;
+
 async function initializeFabricGateway() {
     const wallet = await Wallets.newFileSystemWallet('./wallet');
-    const gateway = new Gateway();
+    gateway = new Gateway();
 
     const connectionProfileJson = fs.readFileSync('./connection-profile.json', 'utf8');
     const connectionProfile = JSON.parse(connectionProfileJson);
 
     await gateway.connect(connectionProfile, { wallet, identity: 'userId' });
-    
-    return { gateway, wallet };
+
+    const network = await gateway.getNetwork('mychannel');  // Replace 'mychannel' with your channel name
+    contract = network.getContract('mychaincode');  // Replace 'mychaincode' with your smart contract name
+
+    return { gateway, wallet, contract };
 }
 
-app.use(express.json());  // Middleware for JSON body parsing
+// Initialize Fabric Gateway
+initializeFabricGateway().then(({ gateway, wallet, contract }) => {
+    // Set Middleware and Routes
+    app.use(express.json()); // Middleware for JSON body parsing
 
-// You can now use 'mydatabase' within your routes or middleware
-// For example, you might pass it as a parameter to the route handlers
+    app.use('/auth', (req, res, next) => {
+        req.db = mydatabase;  // attach the db instance to the request object
+        next();
+    }, authRoutes);
 
-app.use('/auth', (req, res, next) => {
-    req.db = mydatabase;  // attach the db instance to the request object
-    next();
-}, authRoutes);
+    app.use('/files', (req, res, next) => {
+        req.db = mydatabase;  // attach the db instance to the request object
+        req.contract = contract;  // attach the contract to the request object
+        next();
+    }, fileRoutes);
 
-app.use('/files', (req, res, next) => {
-    req.db = mydatabase;  // attach the db instance to the request object
-    next();
-}, fileRoutes);
+    app.post("/auth/signup", (req, res) => {
+        console.log("Signup API was hit");
+        // other code
+    });
 
-app.post("/auth/signup", (req, res) => {
-    console.log("Signup API was hit");
-    // other code
+    const PORT = 3001;
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+}).catch((err) => {
+    console.log('Error initializing Fabric Gateway', err);
 });
-
-const PORT = 3001;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
-
